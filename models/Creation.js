a1778ad9a587e3bb27fb1b76329bf4b1f0e804ac
Matt DeZarn <matt.dezarn@gmail.com>
2021-07-24 12:43:51 -0700

    consolidated all schemas to one 'Creation' Schema

---
+++ models/Creation.js
@@ -0,0 +1,36 @@
+// MODELS Creation.js
+//  Creation model  //
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const creationSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        rarity: { type: String, required: false },
+        weaponType: { type: String, required: false },
+        damageDie: { type: Number, required: false },
+        element: { type: String, required: false },
+        elementDamage: { type: Number, required: false },
+        armorType: { type: String, required: false },
+        armorValue: { type: Number, required: false },
+        itemType: { type: String, required: false },
+        slot: { type: String, required: false },
+        augment: { type: Number, required: false },
+        image: { type: String, required: false },
+        physDescription: { type: String, required: false },
+        propertyDescription: { type: String, required: false },
+
+        posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+
+        votes: [{ type: Schema.Types.ObjectId, ref: 'Vote' }],
+    },
+    {
+        timestamps: false,
+    }
+);
+
+//  model  //
+const Creation = mongoose.model('Creation', creationSchema);
+
+//  export  //
+module.exports = Creation;
