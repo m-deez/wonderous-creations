a1c60817da8ec46c254553830c29cbc5b8b10b44
Matt DeZarn <matt.dezarn@gmail.com>
2021-07-24 16:49:19 -0700

    made changes to get the server to not crash

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const router = require('express').Router();
 const indexControllers = require('../controllers/index');
 
 router.get('/', indexControllers.index);
